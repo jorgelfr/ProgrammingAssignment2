435d3b4fca91e05d027446b6c125333a48b93436
Jorge Luis Figueroa Rodriguez <jorgelfr@users.noreply.github.com>
2015-07-24 10:52:43 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculates the inverse of a matrix. First functions receives and cache the inverse matrix. 
+## Second function verifies if inverse matrix has been calculated, if not inverse matrix is calculated.
 
-## Write a short comment describing this function
+## This function set the matrix mat and then calculates the inverse of matrix mat.
+## Then set the inverse matrix and get the value of the inverse matrix.
 
+
+## function makeCacheMatrix is created an receive as argument a matrix.
 makeCacheMatrix <- function(Mat = matrix())
 {
-     Matinv <- NULL
-     set <- function(y)
+     Matinv <- NULL  ## MatInv is set as a Null element
+     set <- function(y) ## Set function is created with y as an argument
      {
-          Mat <<- y
-          Matinv <<- NULL
+          Mat <<- y  ## Mat is assigned in the parent environment
+          Matinv <<- NULL ## Mat is assigned as null in the parent environment
      }
-     get <- function() Mat
-     setInv <- function(inv) Matinv <<- inv 
-     getInv <- function() Matinv
-     list(set = set, get = get, setInv = setInv, getInv = getInv)
+     get <- function() Mat ## get is assigned with invisible function Mat
+     setInv <- function(inv) Matinv <<- inv ## setInv is assigned with invisible function with argument inv and MatInv is set inv value in the parent environment
+     getInv <- function() Matinv ## getInv is assigned with invisible function MatInv
+     list(set = set, get = get, setInv = setInv, getInv = getInv) ## A list is created with elements, set, get, setInv, getInv with default values
 }
 
 
